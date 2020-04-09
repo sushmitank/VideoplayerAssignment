7bf0f796123b828ba6acc65f16b5a2bbc93e0354
sushmita <sushmitank1994@gmail.com>
2020-04-09 20:37:05 +0530

    First commit

---
+++ app/src/main/java/com/example/videoplayerassignment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.videoplayerassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+String url="https://www.youtube.com/watch?v=cgUBLiF9LwU";
+String url1="https://www.youtube.com/watch?v=fQL4I1-5D4k";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
